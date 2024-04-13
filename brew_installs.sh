17f12d8581aa7d95db6615edd2d1112ebee13c85
atai92 <alan.tai@reddit.com>
2024-04-13 13:27:07 -0700

    brew installs comments for ripgrep and coursier

--- brew_installs.sh
+++ brew_installs.sh
@@ -3,5 +3,7 @@
 set -e
 
 brew install gnu-sed
+# Ripgrep is a faster grep alternative
 brew install ripgrep
+# Install Coursier for Scala support and improve dependency management
 brew install coursier/formulas/coursier && cs setup
